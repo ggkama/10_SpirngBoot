3b6d95883b6b02809b305b5d9e0a3f31e3cc3ee9
ggkama <ggkama@naver.com>
2025-03-31 17:45:12 +0900

    20250331

---
+++ start/src/main/java/com/kh/start/member/model/vo/Member.java
@@ -0,0 +1,22 @@
+package com.kh.start.member.model.vo;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Value;
+
+// Value Object
+// V	 O
+
+
+@Value
+@Getter
+@Builder
+public class Member {
+	
+	private Long memberNo;
+	private String memberId;
+	private String memberPw;
+	private String memberName;
+	private String role;
+	
+}
